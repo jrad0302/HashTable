aea71ca966c5f688c64f97d01d74eab4b0b140ef
Johnathan <jradcliff0302@gmail.com>
2019-11-04 15:34:58 -0500

    init commit

---
+++ hash_table.c
@@ -0,0 +1,303 @@
+/*
+ Student Name:
+ Date:
+
+=======================
+ECE 2035 Project 2-1:
+=======================
+This file provides definition for the structs and functions declared in the
+header file. It also contains helper functions that are not accessible from
+outside of the file.
+
+FOR FULL CREDIT, BE SURE TO TRY MULTIPLE TEST CASES and DOCUMENT YOUR CODE.
+
+===================================
+Naming conventions in this file:
+===================================
+1. All struct names use camel case where the first letter is capitalized.
+  e.g. "HashTable", or "HashTableEntry"
+
+2. Variable names with a preceding underscore "_" will not be called directly.
+  e.g. "_HashTable", "_HashTableEntry"
+
+  Recall that in C, we have to type "struct" together with the name of the struct
+  in order to initialize a new variable. To avoid this, in hash_table.h
+  we use typedef to provide new "nicknames" for "struct _HashTable" and
+  "struct _HashTableEntry". As a result, we can create new struct variables
+  by just using:
+    - "HashTable myNewTable;"
+     or
+    - "HashTableEntry myNewHashTableEntry;"
+
+  The preceding underscore "_" simply provides a distinction between the names
+  of the actual struct defition and the "nicknames" that we use to initialize
+  new structs.
+  [See Hidden Definitions section for more information.]
+
+3. Functions, their local variables and arguments are named with camel case, where
+  the first letter is lower-case.
+  e.g. "createHashTable" is a function. One of its arguments is "numBuckets".
+       It also has a local variable called "newTable".
+
+4. The name of a struct member is divided by using underscores "_". This serves
+  as a distinction between function local variables and struct members.
+  e.g. "num_buckets" is a member of "HashTable".
+
+*/
+
+/****************************************************************************
+* Include the Public Interface
+*
+* By including the public interface at the top of the file, the compiler can
+* enforce that the function declarations in the the header are not in
+* conflict with the definitions in the file. This is not a guarantee of
+* correctness, but it is better than nothing!
+***************************************************************************/
+#include "hash_table.h"
+
+
+/****************************************************************************
+* Include other private dependencies
+*
+* These other modules are used in the implementation of the hash table module,
+* but are not required by users of the hash table.
+***************************************************************************/
+#include <stdlib.h>   // For malloc and free
+#include <stdio.h>    // For printf
+
+
+/****************************************************************************
+* Hidden Definitions
+*
+* These definitions are not available outside of this file. However, because
+* the are forward declared in hash_table.h, the type names are
+* available everywhere and user code can hold pointers to these structs.
+***************************************************************************/
+/**
+ * This structure represents an a hash table.
+ * Use "HashTable" instead when you are creating a new variable. [See top comments]
+ */
+struct _HashTable {
+  /** The array of pointers to the head of a singly linked list, whose nodes
+      are HashTableEntry objects */
+  HashTableEntry** buckets;
+
+  /** The hash function pointer */
+  HashFunction hash;
+
+  /** The number of buckets in the hash table */
+  unsigned int num_buckets;
+};
+
+/**
+ * This structure represents a hash table entry.
+ * Use "HashTableEntry" instead when you are creating a new variable. [See top comments]
+ */
+struct _HashTableEntry {
+  /** The key for the hash table entry */
+  unsigned int key;
+
+  /** The value associated with this hash table entry */
+  void* value;
+
+  /**
+  * A pointer pointing to the next hash table entry
+  * NULL means there is no next entry (i.e. this is the tail)
+  */
+  HashTableEntry* next;
+};
+
+
+/****************************************************************************
+* Private Functions
+*
+* These functions are not available outside of this file, since they are not
+* declared in hash_table.h.
+***************************************************************************/
+/**
+* createHashTableEntry
+*
+* Helper function that creates a hash table entry by allocating memory for it on
+* the heap. It initializes the entry with key and value, initialize pointer to
+* the next entry as NULL, and return the pointer to this hash table entry.
+*
+* @param key The key corresponds to the hash table entry
+* @param value The value stored in the hash table entry
+* @return The pointer to the hash table entry
+*/
+static HashTableEntry* createHashTableEntry(unsigned int key, void* value) {
+  // allocate space for the HashTableEntry
+  HashTableEntry* node = (HashTableEntry*) malloc(sizeof(HashTableEntry));
+  // set the key as the param key
+  node -> key = key;
+  // set the value as the param value
+  node -> value = value;
+  // return the node to the caller
+  return node;
+}
+
+/**
+* findItem
+*
+* Helper function that checks whether there exists the hash table entry that
+* contains a specific key.
+*
+* @param hashTable The pointer to the hash table.
+* @param key The key corresponds to the hash table entry
+* @return The pointer to the hash table entry, or NULL if key does not exist
+*/
+static HashTableEntry* findItem(HashTable* hashTable, unsigned int key) {
+  // find the index using the hash function
+  int index = hashTable -> hash(key);
+  // point to the bucket
+  HashTableEntry** bucket = hashTable -> buckets;
+  // point to the first item in the bucket
+  HashTableEntry* node = bucket[index];
+  // as long as the node is not NULL
+  while(node) {
+    // check for equivalency
+    if (node -> key == key) {
+      // return if keys match
+      return node;
+    } else {
+      // loop
+      node = node -> next;
+    }
+  }
+  // return NULL if not found
+  return NULL;
+}
+
+/****************************************************************************
+* Public Interface Functions
+*
+* These functions implement the public interface as specified in the header
+* file, and make use of the private functions and hidden definitions in the
+* above sections.
+****************************************************************************/
+// The createHashTable is provided for you as a starting point.
+HashTable* createHashTable(HashFunction hashFunction, unsigned int numBuckets) {
+  // The hash table has to contain at least one bucket. Exit gracefully if
+  // this condition is not met.
+  if (numBuckets==0) {
+    printf("Hash table has to contain at least 1 bucket...\n");
+    exit(1);
+  }
+
+  // Allocate memory for the new HashTable struct on heap.
+  HashTable* newTable = (HashTable*)malloc(sizeof(HashTable));
+
+  // Initialize the components of the new HashTable struct.
+  newTable->hash = hashFunction;
+  newTable->num_buckets = numBuckets;
+  newTable->buckets = (HashTableEntry**)malloc(numBuckets*sizeof(HashTableEntry*));
+
+  // As the new buckets contain indeterminant values, init each bucket as NULL.
+  unsigned int i;
+  for (i=0; i<numBuckets; ++i) {
+    newTable->buckets[i] = NULL;
+  }
+
+  // Return the new HashTable struct.
+  return newTable;
+}
+
+void destroyHashTable(HashTable* hashTable) {
+  // create the node
+  HashTableEntry** bucket;
+  HashTableEntry* node;
+  HashTableEntry* temp;
+  // loop through all the num_buckets
+  for (int i = 0; i < hashTable->num_buckets; i++) {
+    bucket = hashTable->buckets;
+    // loop through the nodes in the buckets
+    node = bucket[i];
+    while(node) {
+      // free the node's value and the node
+      temp = node;
+      node = node->next;
+      free(temp->value);
+      free(temp);
+    }
+  }
+  // free the HashTable buckets and HashTable
+  free(hashTable->buckets);
+  free(hashTable);
+}
+
+void* insertItem(HashTable* hashTable, unsigned int key, void* value) {
+  // find the index using the hash function
+  int index = hashTable->hash(key);
+  // point to the bucket
+  HashTableEntry** bucket = hashTable->buckets;
+  // if the bucket's first entry is NULL
+  if(!bucket[index]) {
+    // create a new node
+    HashTableEntry* node = createHashTableEntry(key, value);
+    // place it in the empty bucket
+    bucket[index] = node;
+    // return NULL
+    return NULL;
+  }
+  // else, attempt to find the item
+  HashTableEntry* node = findItem(hashTable, key);
+  HashTableEntry* temp;
+  // if the node is not present (NULL)
+  if(!node) {
+    // loop to end of list
+    node = bucket[index];
+    while(node) {
+      temp = node;
+      node = node->next;
+    }
+    // create new entry at the end of the list
+    temp -> next = createHashTableEntry(key, value);
+    // return NULL
+    return NULL;
+  // else if it returns the pointer of the node matching the key
+  } else {
+    // grab the value from the node
+    void* v = node -> value;
+    // replace the node with a new one
+    node = createHashTableEntry(key, value);
+    // return the old value
+    return v;
+  }
+}
+
+void* getItem(HashTable* hashTable, unsigned int key) {
+  // find the index using the hash function
+  HashTableEntry* node = findItem(hashTable, key);
+  // if the node exists
+  if (node != NULL) {
+    // return the value
+    return node -> value;
+  }
+  // else return NULL
+  return NULL;
+}
+
+void* removeItem(HashTable* hashTable, unsigned int key) {
+  // retrieve the value from the node that matches the key
+  void* v = getItem(hashTable, key);
+  // if it does not exist
+  if(v == NULL) {
+    // return NULL
+    return NULL;
+  } else {
+    // return value otherwise
+    return v;
+  }
+}
+
+void deleteItem(HashTable* hashTable, unsigned int key) {
+  // find the item matching the key
+  HashTableEntry* node = findItem(hashTable, key);
+  // if the node exists
+  if(node) {
+    // free it
+    free(node -> value);
+    free(node);
+  }
+  // otherwise do nothing
+}
